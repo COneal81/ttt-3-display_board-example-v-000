00a73e5156ba3ce069eee296a399662d069ed758
Kari O'Neal <triadfirewood@gmail.com>
2018-06-26 19:54:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def print_and_retuen_display_board
+def display_board
   puts "A Tic Tac Toe Board"
  1.times{puts"   |   |   "}
  1.times{puts"-----------"}
